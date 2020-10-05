7873665fc28a0461bfe292f98d9620fe437def2e
DiogoBotton <diogo.luo@gmail.com>
2020-10-05 17:16:43 -0300

    filtro funcionando :')

--- frontend/scripts/scripts.js
+++ frontend/scripts/scripts.js
@@ -1,6 +1,7 @@
 const ul = document.getElementById('Filmes');
 const url = 'http://localhost:5000/api/Filmes';
 var corpo_tabela = document.querySelector('tbody');
+var tabela = document.querySelector('table');
 
 var filmesTabela = [];
 
@@ -10,18 +11,18 @@ function renderFilmes() {
     myHeaders.append('Content-Type', 'text/json', 'Access-Control-Allow-Origin');
     fetch(url, {
         method: 'get', //método  http para tipo de acesso //delete //post // put,
-        //mode: 'no-cors', // 'cors' by default
         headers: myHeaders
     })
         .then(function (response) {
-
             response.text()
                 .then(function (result) {
                     console.log(response.status)
-                    var jsonResposta = JSON.parse(result);
-                    listar(jsonResposta);
-                    console.log(jsonResposta);
 
+                    var jsonResposta = JSON.parse(result);
+                    filmesTabela = jsonResposta;
+
+                    listar(filmesTabela);
+                    console.log(jsonResposta);
                 })
         })
         .catch(function (err) {
@@ -30,19 +31,34 @@ function renderFilmes() {
 }
 
 function filter() {
+    // Adquire a busca do usuário
     var text = document.getElementById('busca').value;
+    
+    // Verifica se está vazio, caso houver mostra todos os filmes
+    if(text.lenght == 0)
+        listar(filmesTabela)
 
+    // Filter na tabela para buscar filmes pelo nome
     var busca = filmesTabela.filter(function (filme) {
         return filme.titulo.toLowerCase().includes(text.toLowerCase())
     })
 
+    // Método para listagem de filmes
     console.log(busca)
+    listar(busca);
 }
 
 function listar(filmes) {
     // Remove todos os dados da tabela
-    //corpo_tabela.remove('tr');
+    corpo_tabela.remove();
 
+    // Cria um novo corpo de tabela
+    corpo_tabela = document.createElement('tbody')
+
+    // Vincula o novo corpo à tabela
+    tabela.appendChild(corpo_tabela);
+
+    // ForEach em filmes para acrescenta-los linha por linha
     filmes.forEach(filme => {
         // Cria os elementos
         var linha = document.createElement('tr');
@@ -56,24 +72,23 @@ function listar(filmes) {
         var texto_titulo = document.createTextNode(filme.titulo);
         var texto_genero = document.createTextNode(filme.genero);
 
+        // Vincula textos com elementos
+        campo_id.appendChild(texto_id);
+        campo_titulo.appendChild(texto_titulo);
+        campo_genero.appendChild(texto_genero);
+
+        // Vincula campos com a linha
+        linha.appendChild(campo_id);
+        linha.appendChild(campo_titulo);
+        linha.appendChild(campo_genero);
+
+        // Vincula elementos ao documento
+        corpo_tabela.appendChild(linha);
+
         // SOME para verificar se existe alguma referência de ID de filme da tabela (no HTML) com os filmes vindos do backend
-        if (!filmesTabela.some(function (ft) {
-            return ft.idFilme == filme.idFilme;
-        })) {
-            // Vincula textos com elementos
-            campo_id.appendChild(texto_id);
-            campo_titulo.appendChild(texto_titulo);
-            campo_genero.appendChild(texto_genero);
-
-            // Vincula campos com a linha
-            linha.appendChild(campo_id);
-            linha.appendChild(campo_titulo);
-            linha.appendChild(campo_genero);
-
-            // Vincula elementos ao documento
-            corpo_tabela.appendChild(linha);
-        }
+        //if (!filmesTabela.some(function (ft) {
+        //    return ft.idFilme == filme.idFilme;
+        //})) {
+        //}
     });
-
-    filmesTabela = filmes;
 }
